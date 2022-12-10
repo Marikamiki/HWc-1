41b50f18916f40e262401331338c91cfbe170f91
Marikamiki <118117702+Marikamiki@users.noreply.github.com>
2022-12-10 23:04:53 +0300

    Added new folder and did 6 ex

---
+++ ex6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+
+Console.Write("Enter a number : ");
+int numberA = int.Parse(Console.ReadLine());
+
+if (numberA%2 == 0) Console.WriteLine ("even");
+
+else 
+{
+    Console.WriteLine ("odd");
+}
