16ee37482d8dfdc0b1dc392865dc556d84ae5034
Marikamiki <118117702+Marikamiki@users.noreply.github.com>
2022-12-10 23:25:45 +0300

    Added new folder and did 8 ex

---
+++ ex8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.Write("Enter a number : ");
+int numberA = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+Console.WriteLine ($"even numbers from 1 to {numberA}: ");
+
+ while (count < numberA)
+{
+    if (count%2 == 0) 
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
+
+
